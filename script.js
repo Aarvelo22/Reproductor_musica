b2aca02ebc176091f4b23b423c211ce1eaaf4b99
Andres Arvelo <andresarvelo11@gmail.com>
2024-11-08 16:56:17 -0400

    nuevo commit

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 
     const crearCancion = (SongData) => {
-        const div = document.createElement('div');
-        div.setAttribute('class', 'song');
+        const div = document.createElement('div')
+        div.setAttribute('class', 'song')
+        div.innerHTML = ` 
+                <img src="${'https://api.institutoalfa.org/api/songs/image/'+SongData.image.filename}" alt="">
+                <div>
+                    <h4>${SongData.title}</h4>
+                    <p>${SongData.author} </p>
+                </div>`
+
+
+        return div
 
     }
+
+    console.log(axios)
     
+    axios.get('https://api.institutoalfa.org/api/songs')
+    .then((res) => {
+        const canciones = res.data.songs
+
+        canciones.map((cancion) => {
+            document.getElementById("container")
+            .appendChild(crearCancion(cancion))
+
+        })
+    })
     
